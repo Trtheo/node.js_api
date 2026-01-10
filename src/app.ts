cd1f3d946868595c68946b6170a40c6e68e0059d
Theophile NIYIGABA <niyigabatheo10@gmail.com>
2026-01-10 15:42:51 +0200

    use of morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 import categoriesRouter from './routes/categories';
 import productsRouter from './routes/products';
 import cartRouter from './routes/cart';
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(express.json());
 app.use('/api/categories', categoriesRouter);
 app.use('/api/products', productsRouter);
